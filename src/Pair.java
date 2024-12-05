33a9fb75e6d5387ef3e94ff57afe2728e808b350
Javier Martínez Godoy <28204866+jhavimg@users.noreply.github.com>
2024-12-05 18:31:18 +0100

    Delete src/Pair.java

--- src/Pair.java
+++
@@ -1,9 +0,0 @@
-public class Pair {
-    public boolean libre;
-    public int peso;
-    
-    public Pair(boolean libre, int peso){
-        this.libre = libre;
-        this.peso = peso;
-    }
-}
